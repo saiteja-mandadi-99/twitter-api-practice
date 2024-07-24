18828dbb3af00d525811feff79f3d28c3548f207
saiteja-mandadi-99 <saitejamandadi99@gmail.com>
2024-07-24 06:41:57 +0000

    create a user

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const path = require('path')
 const {open} = require('sqlite')
 const sqlite3 = require('sqlite3')
 
+const bcrypt = require('bcrypt')
+const jwtToken = require('jsonwebtoken')
+
 const app = express()
 app.use(express.json())
 
@@ -22,5 +25,28 @@ const initializeDBAndServer = async () => {
 }
 
 initializeDBAndServer()
+
+app.post('/register/', async (request, response) => {
+  const {username, password, name, gender} = request.body
+  try {
+    const api1 = `select * from user where username = '${username}'`
+    const dbUser = await database.get(api1)
+    if (dbUser !== undefined) {
+      response.status(400)
+      response.send('User already exists')
+    } else if (password.length < 6) {
+      response.status(400)
+      response.send('Password is too short')
+    } else {
+      let hashedPassword = await bcrypt.hash(password, 10)
+      const createNewUser = `INSERT INTO USER (username, password, name, gender) values('${username}','${hashedPassword}','${name}','${gender}')`
+      await database.run(createNewUser)
+      response.status(200)
+      response.send('User created successfully')
+    }
+  } catch (e) {
+    console.log(`Error: ${e.message}`)
+  }
+})
 
 module.exports = app
